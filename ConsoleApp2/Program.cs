bd5fd0e6632e04bbce2c0dcb1c6959fc590b4a26
david martinez <al23760329@ite.edu.mx>
2025-04-14 09:13:17 -0700

    agregacion practica ejecucion hilos

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class Program
+{
+    // Método que será ejecutado por los hilos
+    static void Proceso(object id)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Hilo {id}: Iteración {i}");
+            Thread.Sleep(1000); // Simula un proceso que toma tiempo
+        }
+    }
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Inicio del programa...");
+        Console.WriteLine("Flujo unicol..");
+        Stopwatch stopwatch1 = Stopwatch.StartNew();
+        Proceso(1);
+        Proceso(2);
+        Proceso(3);
+        stopwatch1.Stop();
+        Console.WriteLine("Tiempo de ejecución: {0} ms", stopwatch1.ElapsedMilliseconds);
+        Thread hilo1 = new Thread(Proceso);
+        Thread hilo2 = new Thread(Proceso);
+        Thread hilo3 = new Thread(Proceso);
+
+
+        Stopwatch stopwatch2 = Stopwatch.StartNew();
+        hilo1.Start(1);
+        hilo2.Start(2);
+        hilo3.Start(3);
+
+        hilo1.Join();
+        hilo2.Join();
+        hilo3.Join();
+
+        stopwatch2.Stop();
+        Console.WriteLine("Tiempo de ejecución: {0} ms", stopwatch2.ElapsedMilliseconds);
+        Console.WriteLine("Fin del programa...");
+    }
+}
